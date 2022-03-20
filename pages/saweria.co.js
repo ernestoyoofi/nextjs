a0f2704543e7250ced3d666bd63f6823107357b9
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-03-20 19:27:40 +0700

    Add files via upload

---
+++ pages/saweria.co.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+
+// Here you would fetch and return the user
+const useUser = () => ({ user: null, loading: false })
+
+export default function Page() {
+  const { user, loading } = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!(user || loading)) {
+      router.push('https://saweria.co/ernestoyoofi')
+    }
+  }, [user, loading])
+
+  return (
+  <div>
+      <Head>
+          <title>Dukung ernestoyoofi | Saweria</title>
+          <link rel="icon" href="/saweria.png"/>
+      </Head>
+      <body>
+          Reading....
+      </body>
+  </div>
+  )
+}
