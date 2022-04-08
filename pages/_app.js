fe52079796f8f4f57f44f736057d7575212deb23
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-04-08 16:00:18 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( <Component {...pageProps} />
+            <div>
+            <Head>
+              <meta name="google-site-verification" content="wcC28rNmbYV1lR_OamoSTSVfx87bc4xPFDq_yZt62TA" />
+            </Head>
+            </div>
+         )
 }
 
 export default MyApp
